5f3456b87c6b6331a05ad5f6f39c68a86f1ebe3b
Sai Adithya Kothamasu <adithya261004@gmail.com>
2024-09-27 23:08:21 +0530

    routing fixed and added vercel override

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
@@ -12,6 +12,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signup"/>} />
           <Route path="/signup" element={<Signup/>} />
           <Route path="/signin" element={<Signin/>} />
           <Route path="/blog/:id" element={<Blog/>} />
